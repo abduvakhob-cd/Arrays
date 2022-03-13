8cb1b6f11ad532734ec2dc5bcb6d5420c18ed619
Abduvakhob Abdurazzakov <abduvakhobabdurazzakov@MacBook-Pro.local>
2022-03-14 03:58:56 +0900

    ls

---
+++ script.js
@@ -0,0 +1,162 @@
+// function logger() {
+//   console.log("my name is jonas");
+// }
+
+// // calling or running / invoking function
+// logger();
+// logger();
+// logger();
+
+// function fruitProcessor(apples, oranges) {
+//   console.log(apples, oranges);
+//   const juice = `Juice with ${apples} apples and ${oranges}
+//   oranges.`;
+//   return juice;
+// }
+
+// const appleJuice = fruitProcessor(8989, 12);
+// console.log(appleJuice);
+
+// const appleOrangeJuice = fruitProcessor(2, 4);
+// console.log(appleOrangeJuice);
+
+// const num = Number("23");
+
+// function logger() {
+//   console.log("my name is Jonas ");
+// }
+// // calling/running invoking
+// logger();
+// logger();
+// logger();
+
+// function fruitProcessor(apple, orange) {
+//   const juice = `juice with ${apple} apples and ${orange} oranges.`;
+//   return juice;
+// }
+
+// const appleJuice = fruitProcessor(12, 122);
+// console.log(appleJuice);
+
+// const appleOrangeJuice = fruitProcessor(221, 1212);
+// console.log(appleOrangeJuice);
+
+// function calcAge1(birthYear) {
+// return 2037 - birthYear;
+// }
+// const age1 = calcAge1(1991);
+// console.log(age1);
+
+"use strict";
+// let hasDriversLicense = false;
+// const passTest = true;
+
+// if (passTest) hasDriversLicense = true;
+// if (hasDriversLicense) console.log(`I can drive`);
+
+// function logger() {
+//   console.log(`i am abu`);
+// }
+
+// logger();
+// logger();
+// logger();
+// function fruitProcessor(apples, oranges) {
+//   console.log(apples, oranges);
+//   const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+//   return juice;
+// }
+
+// const appleJuice = fruitProcessor(23, 2);
+// console.log(appleJuice);
+
+// const appleOrangeJuice = fruitProcessor(2, 4);
+// console.log(appleOrangeJuice);
+
+// function logitech() {
+//   console.log(`this is my example`);
+// }
+
+// logitech();
+
+// function describeCountry(Country, population, capitalcity) {
+//   console.log(Country, population, capitalcity);
+//   const nam = `${Country} has ${population} million people and its capital city is ${capitalcity} `;
+//   return nam;
+// }
+
+// const namexample = describeCountry(uzbekista, 22, tash);
+// console.log(namexample);
+
+// function describeCountry(Country, population, capital) {
+//   return `${Country} has ${population} million people and its capitalcity is ${capital}  `;
+// }
+
+// const descKorea = describeCountry(`Korea`, 12, `Seoul`);
+// const descUSA = describeCountry(`US`, 350, `Washington.DC`);
+// const descUZ = describeCountry(`Uzbekistan`, 33, `TAshkent`);
+// console.log(descKorea, descUSA, descUZ);
+// function calcAge1(birthYear) {
+//   return 2037 - birthYear;
+// }
+
+// const age1 = calcAge1(1991);
+// console.log(age1);
+
+// const calcage2 = function (birthYear) {
+//   return 2037 - birthYear;
+// }
+
+// 'use strict';
+//  const calcAge3 = birthYeah => 2037 - birthYeah;
+//  const age3 = calcAge3(1991);
+//  console.log(age3);
+
+// const yearsUntilRetirement = (birthYeah, firstName)
+//  {
+//   const age = 2037 - birthYeah;
+//   const retirement = 65 - age;
+//   return `${firstName} retires in ${retirement} years`;
+// }
+
+// console.log(yearsUntilRetirement(1991, 'Jonas'));
+// console.log(yearsUntilRetirement(1911, 'sara'));
+
+// function cutFruitPieces(  )
+// function fruitProcessor(apple, orange) {
+//   const juice = `Juice with ${apple} apples and ${orange} oranges`;
+//   return juice;
+// }
+
+// const calcAge = function (birthyeah) {
+//   return 2037 - birthyeah;
+// };
+// const yearsUntilRetirement = function (birthyeah, firstName) {
+//   const age = calcAge(birthyeah);
+//   const retirement = 65 - age;
+//   return `${firstName} retires in ${retirement} years`;
+// };
+
+// console.log(earsUntilRetirement(1991, "sara"));
+
+// const friend1 = `sara`;
+// const friend2 = `hyine`;
+// const friend3 = `jim`;
+// const friends = [`sara`, `hyine`, `jim`];
+
+// console.log(friends);
+
+// const years = new Array(1991, 6767, 78676, 7676, 7878);
+// console.log(friends[0]);
+// console.log(friends[2]);
+
+// friends[2] = `jay`;
+
+// const firstName = `jonas`;
+// const jonas = [firstName, `jonas`, `scate`, 2037 - 1991, ` teacher`, friends];
+// console.log(jonas);
+
+const calcAge = function (birthYeah) {
+  return 2037 - birthYeah;
+};
+const years = [1990, 1967, 2002];
