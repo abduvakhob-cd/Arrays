64e683ead0425c35b83dc1f1aa3862422c8f7f99
Abduvakhob Abdurazzakov <abduvakhobabdurazzakov@MacBook-Pro.local>
2022-03-16 09:09:54 +0900

    Loop

---
+++ loop.js
@@ -0,0 +1,13 @@
+console.log(`Lifting weight repetition 1`);
+console.log(`Lifting weight repetition 2`);
+console.log(`Lifting weight repetition 3`);
+console.log(`Lifting weight repetition 4`);
+console.log(`Lifting weight repetition 5`);
+console.log(`Lifting weight repetition 6`);
+console.log(`Lifting weight repetition 7`);
+console.log(`Lifting weight repetition 8`);
+console.log(`Lifting weight repetition 9`);
+
+for (let rep = 11; rep <= 13; rep++) {
+  console.log(`Lifting weight repetition ${rep}`);
+}
